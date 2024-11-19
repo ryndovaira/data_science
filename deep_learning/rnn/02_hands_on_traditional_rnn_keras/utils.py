79bfcf4b9444abdbb37d4b60cdd02ff773fc8ffb
Ryndova Irina <ryndovaira@gmail.com>
2024-11-19 21:38:29 +0300

    refactor utils.py

--- deep_learning/rnn/02_hands_on_traditional_rnn_keras/utils.py
+++ deep_learning/rnn/02_hands_on_traditional_rnn_keras/utils.py
@@ -2,22 +2,22 @@ import logging
 import os
 from datetime import datetime
 import matplotlib.pyplot as plt
-
 from config import Config
 
 
 def get_timestamp() -> str:
+    """Returns the current timestamp as a string."""
     return datetime.now().strftime("%y%m%d_%H%M%S")
 
 
 def setup_logger():
     """Sets up a logger that writes to a specified log file with a unique timestamp."""
-    # Create logs directory if it doesn't exist
+    # Define the log directory path
     log_dir = os.path.join(os.getcwd(), Config.LOG_DIR, Config.MODE)
-    if not os.path.exists(log_dir):
-        os.makedirs(log_dir)
+    # Create the log directory if it doesn't exist
+    os.makedirs(log_dir, exist_ok=True)
 
-    # Add a timestamp to the log file name
+    # Define the log file path with a timestamp
     log_file_path = os.path.join(log_dir, f"log_{get_timestamp()}.log")
 
     # Create and configure the logger
@@ -37,35 +37,39 @@ def setup_logger():
 
 def save_artifacts(history, model, output_dir):
     """Saves training history and model artifacts with unique timestamps."""
-    # Create output directory if it doesn't exist
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
+    # Create the output directory if it doesn't exist
+    os.makedirs(output_dir, exist_ok=True)
 
-    # Save the model with a unique filename
+    # Define the model filename with a timestamp
     model_filename = f"model_{get_timestamp()}.keras"
+    # Save the model to the specified directory
     model.save(os.path.join(output_dir, Config.MODE, model_filename))
 
 
 def checkpoint_path():
-    # Generate a unique base filename with timestamp
+    """Generates a unique checkpoint path with a timestamp."""
+    # Define the checkpoint directory path
     checkpoint_dir = os.path.join(Config.ARTIFACTS_DIR, Config.MODE)
+    # Create the checkpoint directory if it doesn't exist
+    os.makedirs(checkpoint_dir, exist_ok=True)
 
-    # Create output directory if it doesn't exist
-    if not os.path.exists(checkpoint_dir):
-        os.makedirs(checkpoint_dir)
-
-    s = f"model_checkpoint_{get_timestamp()}" + "_epoch-{epoch:02d}_val-loss-{val_loss:.4f}.keras"
-    return os.path.join(checkpoint_dir, s)
+    # Define the checkpoint file path with placeholders for epoch and validation loss
+    return os.path.join(
+        checkpoint_dir,
+        f"model_checkpoint_{get_timestamp()}_epoch-{{epoch:02d}}_val-loss-{{val_loss:.4f}}.keras",
+    )
 
 
 def plot_history(history):
     """Plots and saves the training history as an image file."""
-    # Create logs directory if it doesn't exist
+    # Define the save directory path
     save_dir = os.path.join(os.getcwd(), Config.PLOT_DIR, Config.MODE)
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
+    # Create the save directory if it doesn't exist
+    os.makedirs(save_dir, exist_ok=True)
+    # Define the save file path with a timestamp
     save_file_path = os.path.join(save_dir, f"plot_{get_timestamp()}.png")
 
+    # Create a new figure for the plot
     plt.figure(figsize=(10, 6))
     # Plot training and validation loss
     plt.plot(history.history["loss"], label="Training Loss")
@@ -93,11 +97,13 @@ def plot_history(history):
             label="Test Accuracy",
         )
 
+    # Add title and labels to the plot
     plt.title("Training, Validation, and Test Loss/Accuracy")
     plt.xlabel("Epoch")
     plt.ylabel("Loss/Accuracy")
     plt.legend()
     plt.grid(True)
 
+    # Save the plot to the specified file
     plt.savefig(save_file_path)
     plt.close()
