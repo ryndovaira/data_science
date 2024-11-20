97c127b5cd952d09c10c2b6e27ac7bb37f4ab806
Ryndova Irina <ryndovaira@gmail.com>
2024-11-20 18:00:35 +0300

    styling

--- deep_learning/rnn/02_hands_on_traditional_rnn_keras/model.py
+++ deep_learning/rnn/02_hands_on_traditional_rnn_keras/model.py
@@ -2,11 +2,14 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Embedding, SimpleRNN, Dense
 from config import Config
 
+
 def build_rnn_model(input_length):
-    model = Sequential([
-        Embedding(Config.MAX_FEATURES, Config.EMBEDDING_DIM, input_length=input_length),
-        SimpleRNN(Config.RNN_UNITS),
-        Dense(1, activation='sigmoid')
-    ])
-    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    model = Sequential(
+        [
+            Embedding(Config.MAX_FEATURES, Config.EMBEDDING_DIM, input_length=input_length),
+            SimpleRNN(Config.RNN_UNITS),
+            Dense(1, activation="sigmoid"),
+        ]
+    )
+    model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
     return model
