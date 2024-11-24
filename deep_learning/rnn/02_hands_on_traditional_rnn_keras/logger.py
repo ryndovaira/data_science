ef5d1210d61ad476305030c56be25c4bf67e6f46
Ryndova Irina <ryndovaira@gmail.com>
2024-11-24 17:15:06 +0300

    add StreamHandler logger

--- deep_learning/rnn/02_hands_on_traditional_rnn_keras/logger.py
+++ deep_learning/rnn/02_hands_on_traditional_rnn_keras/logger.py
@@ -14,16 +14,25 @@ def setup_logger():
     # Define the log file path with a timestamp
     log_file_path = os.path.join(save_dir, f"log_{Config.TIMESTAMP}.txt")
 
+    log_format = "%(asctime)s - %(levelname)s - %(message)s"
+
     # Create and configure the logger
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
     handler = logging.FileHandler(log_file_path)
     handler.setLevel(logging.INFO)
-    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    formatter = logging.Formatter(log_format)
     handler.setFormatter(formatter)
+
+    # Stream handler for console output
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.INFO)
+    stream_formatter = logging.Formatter(log_format)
+    stream_handler.setFormatter(stream_formatter)
 
     # Avoid adding multiple handlers if the logger is reused
     if not logger.handlers:
         logger.addHandler(handler)
+        logger.addHandler(stream_handler)
 
     return logger
